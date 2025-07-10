926593471ba354228de0add1e93cfce8da27c826
guilhermesvm <guilhermesvmachado@gmail.com>
2025-07-09 21:41:27 -0300

    fix: fixing variable names

--- JornadaMilhas/playwright/e2e/Pages/LoginPage.js
+++ JornadaMilhas/playwright/e2e/Pages/LoginPage.js
@@ -4,33 +4,25 @@ export class LoginPage {
     
     constructor(page){
         this.page = page;
-    }
-
-    get fieldEmail() {
-        return this.page.getByTestId('input-email');
-    }
-
-    get fieldPassword() {
-        return this.page.getByTestId('input-senha');
-    }
-
-    get submitButton(){
-        return this.page.getByTestId('botao-acessar-conta')
+        this.pageHeader = page.getByText('Login', { exact: true });
+        this.emailField = page.getByTestId('input-email');
+        this.passwordField = page.getByTestId('input-senha');
+        this.submitButton = page.getByTestId('botao-acessar-conta');
     }
 
     async visitPage(){
         await this.page.goto('/auth/login');
-        await expect(this.page).toHaveURL('/auth/login')
-        const pageHeader = this.page.getByText('Login', { exact: true });
-        await expect(pageHeader).toHaveText('Login');
+        await expect(this.page).toHaveURL('/auth/login');
+        await expect(this.pageHeader).toBeVisible();
+        await expect(this.pageHeader).toHaveText('Login');
     }
 
     async fillEmail(email){
-        await this.fieldEmail.fill(email);
+        await this.emailField.fill(email);
     }
 
     async fillPassword(password){
-        await this.fieldPassword.fill(password);
+        await this.passwordField.fill(password);
     }
 
     async clickSubmitButton(){
@@ -38,8 +30,8 @@ export class LoginPage {
     }
 
     async fillAndSubmitLoginForm(email, password){
-        await this.fieldEmail.fill(email);
-        await this.fieldPassword.fill(password);
+        await this.emailField.fill(email);
+        await this.passwordField.fill(password);
         if(await this.submitButton.isEnabled()){
             await this.clickSubmitButton();
         }   
