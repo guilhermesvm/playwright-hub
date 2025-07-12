a3d0e0fa353ee0826b0aeac6c29d7eee41476863
guilhermesvm <guilhermesvmachado@gmail.com>
2025-07-11 22:41:55 -0300

    feat: complete happy one-way ticket search flow

--- JornadaMilhas/playwright/e2e/Pages/HomePage.js
+++ JornadaMilhas/playwright/e2e/Pages/HomePage.js
@@ -5,14 +5,23 @@ export class HomePage {
     constructor(page){
         this.page = page;
         this.pageHeader = page.getByTestId('titulo-passagens');
-        this.oneWayButton = page.getByRole('button', { name: 'SOMENTE IDA' });
-        this.passengerModal = page.getByTestId('abrir-modal-passageiros');
+        this.oneWayButton = page.getByTestId('botao-somente-ida');
+        this.openPassengerModalBtn = page.getByTestId('abrir-modal-passageiros');
         this.incrementAdultBtn = page.getByTestId('seletor-passageiro-adultos').getByRole('button', { name: 'Ícone do operador de adição' });
-        this.incrementKidsBtn = page.getByTestId('seletor-passageiro-criancas').getByRole('button', { name: 'Ícone do operador de adição' });
-        this.incrementBabyBtn = page.getByTestId('seletor-passageiro-bebes').getByRole('button', { name: 'Ícone do operador de adição' });
+        this.incrementChildBtn = page.getByTestId('seletor-passageiro-criancas').getByRole('button', { name: 'Ícone do operador de adição' });
+        this.incrementInfantBtn = page.getByTestId('seletor-passageiro-bebes').getByRole('button', { name: 'Ícone do operador de adição' })
+        this.closePassengerModalBtn = page.getByTestId('fechar-modal-passageiros');
         this.dropdownFieldOrigin = page
+        
             .getByTestId('campo-dropdown-origem')
             .getByLabel('Origem');
+            
+        this.dropdownFieldDestination = page
+            .getByTestId('campo-dropdown-destino')
+            .getByLabel('Destino');
+
+        this.departureDateField = page.getByTestId('campo-data-ida');
+        this.searchButton = page.getByTestId('botao-buscar-passagens');
     }
 
     async visitPage(){
@@ -22,12 +31,51 @@ export class HomePage {
         await expect(this.pageHeader).toHaveText('Passagens');
     }
 
-    async openPassengerModal(){
-        await this.passengerModal.click();
-        const adultCount = this.page.getByText('1', { exact: true });
-        await expect(adultCount).toBeVisible();
-        await this.incrementAdultBtn.click();
-        await expect(this.page.getByText('2', { exact: true })).toBeVisible();
-
+    async clickOneWayButton(){
+        await this.oneWayButton.click();
     }
+
+    async openPassengerModal(){
+        await this.openPassengerModalBtn.click();
+    }
+
+    async setAdultPassenger(quantity){
+        for(let i = 1; i < quantity; i++){
+            await this.incrementAdultBtn.click();
+        }
+    
+    }
+
+    async setChildPassenger(quantity){
+        for(let i = 0; i < quantity; i++){
+            await this.incrementChildBtn.click();
+        }
+    }
+
+     async setInfantPassenger(quantity){
+        for(let i = 0; i < quantity; i++){
+            await this.incrementInfantBtn.click();
+        }
+    }
+    async closePassengerModal(){
+        await this.closePassengerModalBtn.click();
+    }
+
+    async setOriginAndDestination(origin, destination){
+        await this.dropdownFieldOrigin.fill(origin);
+        await this.dropdownFieldOrigin.press('Enter');
+
+        await this.dropdownFieldDestination.fill(destination);
+        await this.dropdownFieldDestination.press('Enter');
+    }
+
+    async setDepartureDate(date){
+        const formattedDate = new Intl.DateTimeFormat('en-US').format(date)
+        await this.departureDateField.fill(formattedDate);
+    }
+
+    async searchForTickets(){
+        await this.searchButton.click();
+    }
+
 }
--- JornadaMilhas/playwright/e2e/searchTickets.spec.js
+++ JornadaMilhas/playwright/e2e/searchTickets.spec.js
@@ -5,6 +5,18 @@ test.describe('Search Plane Tickets', () => {
     test('Should only search for one-way ticket', async ({ page }) => {
         const homePage = new HomePage(page);
         await homePage.visitPage();
+        await homePage.clickOneWayButton();
+
         await homePage.openPassengerModal();
+        await homePage.setAdultPassenger(3);
+        await homePage.setChildPassenger(2);
+        await homePage.setInfantPassenger(2);
+        await homePage.closePassengerModal();
+
+        const origin = 'rio grande do sul';
+        const destination  = 'santa catarina';
+        await homePage.setOriginAndDestination(origin, destination);
+        await homePage.setDepartureDate(new Date());
+        await homePage.searchForTickets();
     })
 })
