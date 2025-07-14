861d4cb967dd05bfdc65325e18896456fef61cab
guilhermesvm <guilhermesvmachado@gmail.com>
2025-07-14 13:57:58 -0300

    feat: new Page: SearchPage

---
+++ JornadaMilhas/playwright/e2e/Pages/SearchPage.js
@@ -0,0 +1,21 @@
+import { expect } from "@playwright/test";
+
+export class SearchPage {
+    constructor(page){
+        this.page = page;
+        this.oneWayText = page.getByTestId('texto-ida-volta')
+        this.originContainer = page.getByTestId('container-origem');
+        this.destinationContainer = page.getByTestId('container-destino');
+        this.purchaseButton = page.getByTestId('botao-comprar');
+    }
+
+    async isDisplayingTrip(ticketType, origin, destination){
+        await expect(this.oneWayText).toHaveText(ticketType);
+        await expect(this.originContainer).toContainText(origin);
+        await expect(this.destinationContainer).toContainText(destination);
+        await expect(this.purchaseButton).toBeVisible();
+        await expect(this.purchaseButton).toBeEnabled();
+    }
+
+
+}
