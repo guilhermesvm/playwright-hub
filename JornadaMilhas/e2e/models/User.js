5901170d75a68755f6f72a241eb598e722d50eaa
guilhermesvm <guilhermesvmachado@gmail.com>
2025-07-16 14:40:31 -0300

    feat: create User model

---
+++ JornadaMilhas/e2e/models/User.js
@@ -0,0 +1,19 @@
+export const gender = Object.freeze({
+    MALE: 'Masculino',
+    FEMALE: 'Feminino',
+    OTHER: 'Prefiro não informar'
+});
+
+export class User {
+    constructor({ name, birthDate, gender, cpf, phone, city, state, email, password }){
+        this.name = name;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.cpf = cpf;
+        this.phone = phone;
+        this.city = city;
+        this.state = state;
+        this.email = email;
+        this.password = password;
+    }
+}
