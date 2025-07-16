8de0aef2059f1c3f8dea7df60452e3aa3d629dbf
guilhermesvm <guilhermesvmachado@gmail.com>
2025-07-16 14:40:09 -0300

    feat: add userFactory for creating random users

---
+++ JornadaMilhas/e2e/factories/userFactory.js
@@ -0,0 +1,19 @@
+import { User } from "../models/User";
+import { Faker, pt_BR } from "@faker-js/faker";
+import { gender } from "../models/User";
+
+const faker = new Faker({locale: pt_BR})
+
+export function generateRandomUser(){
+    return new User({
+        name: faker.person.fullName(),
+        birthDate: faker.date.birthdate(),
+        gender: faker.helpers.enumValue(gender),
+        cpf: faker.string.numeric(11),
+        phone: faker.string.numeric(11),
+        city: faker.location.city(),
+        state: faker.location.state(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+    })
+}
